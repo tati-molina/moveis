f7031ec8af5109e7d33446f66ea82161faf15e99
Tatiana Castro <2021000884@svs.iffarroupilha.local>
2024-08-15 20:13:18 -0300

    Inserindo o arquivo principal.js

---
+++ principal.js
@@ -0,0 +1,9 @@
+var banners = ["Os melhores do Brasil", "Qualidade e preço baixo!"];
+var banner = 0;
+
+function trocaBanner(){
+    banner = (banner +1) % 2;
+    document.querySelector("h2#mensagem").textContent = banners[banner];
+}
+
+setInterval(trocaBanner, 2000);
